90f271bfe518ef0ca5a48cef80e69764cac7dc58
kamiI-suchy <kamilsuchy132@gmail.com>
2026-05-31 20:08:34 +0200

    Pelny redesign: teal accent, cienie, GitHub-dark kod, animacje kart

--- js/app.js
+++ js/app.js
@@ -234,7 +234,7 @@ function renderTask() {
           '</div>';
 
   // pdf link
-  html += '<a class="pdf-link" href="' + task.pdf + '" target="_blank">Instrukcja PDF</a>';
+  html += '<a class="pdf-link" href="' + task.pdf + '" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/></svg>Instrukcja PDF</a>';
 
   // uruchomienie accordion – loads from file
   if (task.uruchomienie) {
