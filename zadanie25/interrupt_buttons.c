c2c53917ff19a93914eac6e4602aa1ba63766569
kamiI-suchy <kamilsuchy132@gmail.com>
2026-05-31 21:25:45 +0200

    Poprawa polskich znaków diakrytycznych w interrupt_buttons.c

--- zadanie25/interrupt_buttons.c
+++ zadanie25/interrupt_buttons.c
@@ -7,7 +7,7 @@
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Kamil Suchy");
-MODULE_DESCRIPTION("Sterownik przerwan dla przyciskow GPIO68 (zolty) i GPIO69 (niebieski)");
+MODULE_DESCRIPTION("Sterownik przerwań dla przycisków GPIO68 (żółty) i GPIO69 (niebieski)");
 
 #define GPIO2_START_ADDR        0x481AC000
 #define GPIO2_SIZE              0x1000
@@ -32,13 +32,13 @@ static int irq_blue;
 
 static irqreturn_t button_yellow_isr(int irq, void *dev_id)
 {
-	printk(KERN_INFO "Wcisniety przycisk GPIO68 (zolty)\n");
+	printk(KERN_INFO "Wciśnięty przycisk GPIO68 (żółty)\n");
 	return IRQ_HANDLED;
 }
 
 static irqreturn_t button_blue_isr(int irq, void *dev_id)
 {
-	printk(KERN_INFO "Wcisniety przycisk GPIO69 (niebieski)\n");
+	printk(KERN_INFO "Wciśnięty przycisk GPIO69 (niebieski)\n");
 	return IRQ_HANDLED;
 }
 
@@ -49,11 +49,11 @@ static int __init interrupt_init(void)
 	void __iomem *mem;
 	void __iomem *cm_per;
 
-	printk(KERN_INFO "Ladowanie modulu przerwan...\n");
+	printk(KERN_INFO "Ładowanie modułu przerwań...\n");
 
 	cm_per = ioremap(CM_PER_START_ADDR, CM_PER_SIZE);
 	if (!cm_per) {
-		printk(KERN_ERR "Blad ioremap CM_PER\n");
+		printk(KERN_ERR "Błąd ioremap CM_PER\n");
 		return -ENOMEM;
 	}
 	iowrite32(0x02, cm_per + CM_PER_GPIO2_CLKCTRL);
@@ -61,27 +61,27 @@ static int __init interrupt_init(void)
 
 	ret = gpio_request_one(BUTTON_YELLOW, GPIOF_IN, "btn_yellow");
 	if (ret) {
-		printk(KERN_ERR "Blad gpio_request dla GPIO68: %d\n", ret);
+		printk(KERN_ERR "Błąd gpio_request dla GPIO68: %d\n", ret);
 		return ret;
 	}
 
 	ret = gpio_request_one(BUTTON_BLUE, GPIOF_IN, "btn_blue");
 	if (ret) {
-		printk(KERN_ERR "Blad gpio_request dla GPIO69: %d\n", ret);
+		printk(KERN_ERR "Błąd gpio_request dla GPIO69: %d\n", ret);
 		gpio_free(BUTTON_YELLOW);
 		return ret;
 	}
 
 	irq_yellow = gpio_to_irq(BUTTON_YELLOW);
 	if (irq_yellow < 0) {
-		printk(KERN_ERR "Blad gpio_to_irq GPIO68: %d\n", irq_yellow);
+		printk(KERN_ERR "Błąd gpio_to_irq GPIO68: %d\n", irq_yellow);
 		ret = irq_yellow;
 		goto err_gpio;
 	}
 
 	irq_blue = gpio_to_irq(BUTTON_BLUE);
 	if (irq_blue < 0) {
-		printk(KERN_ERR "Blad gpio_to_irq GPIO69: %d\n", irq_blue);
+		printk(KERN_ERR "Błąd gpio_to_irq GPIO69: %d\n", irq_blue);
 		ret = irq_blue;
 		goto err_gpio;
 	}
@@ -89,21 +89,21 @@ static int __init interrupt_init(void)
 	ret = request_irq(irq_yellow, button_yellow_isr,
 			  IRQF_TRIGGER_FALLING, "btn_yellow", NULL);
 	if (ret) {
-		printk(KERN_ERR "Blad request_irq GPIO68: %d\n", ret);
+		printk(KERN_ERR "Błąd request_irq GPIO68: %d\n", ret);
 		goto err_gpio;
 	}
 
 	ret = request_irq(irq_blue, button_blue_isr,
 			  IRQF_TRIGGER_FALLING, "btn_blue", NULL);
 	if (ret) {
-		printk(KERN_ERR "Blad request_irq GPIO69: %d\n", ret);
+		printk(KERN_ERR "Błąd request_irq GPIO69: %d\n", ret);
 		free_irq(irq_yellow, NULL);
 		goto err_gpio;
 	}
 
 	mem = ioremap(GPIO2_START_ADDR, GPIO2_SIZE);
 	if (!mem) {
-		printk(KERN_ERR "Blad ioremap GPIO2\n");
+		printk(KERN_ERR "Błąd ioremap GPIO2\n");
 		ret = -ENOMEM;
 		goto err_irq;
 	}
@@ -126,7 +126,7 @@ static int __init interrupt_init(void)
 
 	iounmap(mem);
 
-	printk(KERN_INFO "Modul przerwan zaladowany (GPIO68 IRQ=%d, GPIO69 IRQ=%d)\n",
+	printk(KERN_INFO "Moduł przerwań załadowany (GPIO68 IRQ=%d, GPIO69 IRQ=%d)\n",
 	       irq_yellow, irq_blue);
 	return 0;
 
@@ -145,7 +145,7 @@ static void __exit interrupt_exit(void)
 	free_irq(irq_blue, NULL);
 	gpio_free(BUTTON_YELLOW);
 	gpio_free(BUTTON_BLUE);
-	printk(KERN_INFO "Modul przerwan wyladowany\n");
+	printk(KERN_INFO "Moduł przerwań wyładowany\n");
 }
 
 module_init(interrupt_init);
